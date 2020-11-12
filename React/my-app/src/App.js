30d9a8a4bd56768e9947c4b44d5f9a04e3a87121
sherzach <goblix9@gmail.com>
2020-11-12 20:29:58 +0000

    React webpage challenge in class

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -6,15 +6,33 @@ import { jsx, css } from "@emotion/core"
 
 const name = 'Weeeeeeee!!!!!'
 // https://www.theaicore.com/wp-content/uploads/2020/11/Screenshot-2020-11-01-at-15.13.05.png
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src="Ai_core_logo.png" className="App-logo" alt="AICoreLogo" css={css`height:50; width: 50`}/>
+      <header>
+        <div className="container_pink">
+          <div className="item">
+          </div>
+          <div className="item">
+          </div>
+        </div>   
       </header>
-      <body css={css`background-color: #282c34; min-height: 100vh`}>
-        <div css={css`color: dodgerblue; font-size: 40px`} className="Text-spin">
-          {name}
+      <body>
+        <div classname="big_box">
+          <div className="bottom_box">
+            <div className="pink_bot_box">
+              <div className="green_box">box</div>
+              <div className="green_box">box</div>
+            </div>
+            <div className="pink_bot_box">
+              <div className="blue_box">box</div>
+              <div className="blue_box">box</div>
+            </div>
+          </div>
+          <div className="bottom_blue_box">
+            box
+          </div>
         </div>
       </body>
     </div>
