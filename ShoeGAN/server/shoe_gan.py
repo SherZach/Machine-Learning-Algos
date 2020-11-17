2d7adcc43c10515774cb92d9f0cfd37fa46862cf
sherzach <goblix9@gmail.com>
2020-11-17 00:00:34 +0000

    Figured out how to get image from url

--- ShoeGAN/server/shoe_gan.py
+++ ShoeGAN/server/shoe_gan.py
@@ -3,10 +3,17 @@
 import pandas as pd
 import glob
 
-path = r'C:\Users\gobli\Documents\AI CORE\Machine-Learning-Algos\ShoeGAN\server\data'
-all_files = glob.glob(os.path.join(path, "*.txt")) 
+# path = r'C:\Users\gobli\Documents\AI CORE\Machine-Learning-Algos\ShoeGAN\server\data'
+# all_files = glob.glob(os.path.join(path, "*.txt")) 
 
-df_from_each_file = [pd.read_csv(f) for f in all_files]
+# df_from_each_file = [pd.read_csv(f) for f in all_files]
 
-concatenated_df = pd.concat(df_from_each_file, axis=0, ignore_index=True)
+# concatenated_df = pd.concat(df_from_each_file, axis=0, ignore_index=True)
+adidas_raw = pd.read_csv("data/adidas.txt", sep="\r")
+adidas_raw.head()
 
+#%%
+import requests
+
+r = requests.get(adidas_raw.iloc[1][0])
+print(r)
