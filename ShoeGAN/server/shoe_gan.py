9d74313085a89a3cbab45c8ad3b7e20fd260dbc7
sherzach <goblix9@gmail.com>
2020-11-18 22:33:52 +0000

    updated git ignore

--- ShoeGAN/server/shoe_gan.py
+++ ShoeGAN/server/shoe_gan.py
@@ -2,18 +2,30 @@
 
 import pandas as pd
 import glob
+import os
 
-# path = r'C:\Users\gobli\Documents\AI CORE\Machine-Learning-Algos\ShoeGAN\server\data'
-# all_files = glob.glob(os.path.join(path, "*.txt")) 
+path = r'C:\Users\gobli\Documents\AI CORE\Machine-Learning-Algos\ShoeGAN\server\data'
+all_files = glob.glob(os.path.join(path, "*.txt")) 
 
-# df_from_each_file = [pd.read_csv(f) for f in all_files]
+df_from_each_file = [pd.read_csv(f, sep="\r",names=["Link"]) for f in all_files]
+# print(df_from_each_file)
 
-# concatenated_df = pd.concat(df_from_each_file, axis=0, ignore_index=True)
-adidas_raw = pd.read_csv("data/adidas.txt", sep="\r")
-adidas_raw.head()
-
+total_df = pd.concat(df_from_each_file, ignore_index=True)
+total_df
+print(total_df.iloc[46][0])
 #%%
 import requests
 
-r = requests.get(adidas_raw.iloc[1][0])
-print(r)
+for i in range(len(total_df)):
+    try:
+        r = requests.get(total_df.iloc[i][0], timeout=10)
+    except:
+        print("Problem:", i)
+        continue
+
+    if r.ok:
+        print("Sucess:", i)
+        with open("data/images/" + str(i) + ".jpg", "wb") as f:
+            f.write(r.content)
+# Do not run this cell again unless you want to waste 10 minutes!
+#%%
